b055b222810aa0dda14a32ffb3294e61e616dd56
sanine <sanine.not@pm.me>
2026-07-10 13:02:03 -0500

    add basic led animations

--- robot-arm-controller/js_firmware/js_firmware.ino
+++ robot-arm-controller/js_firmware/js_firmware.ino
@@ -1,28 +1,160 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+#include "pins.h"
 
 
-#define J1_X A1
-#define J1_Y A0
-// #define J2_X A3
-#define J2_Y A2
+#define LED_COUNT 57
+#define LED_BRIGHTNESS 50
+Adafruit_NeoPixel led_strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
 
-#define B_UP 2
-#define B_DOWN 3
 
+// --===== LED disks & animations =====--
+
+struct LedDisk;
+#define OUTER_DISK_LEN 12
+#define INNER_DISK_LEN 6
+#define TOTAL_DISK_LEN (OUTER_DISK_LEN + INNER_DISK_LEN + 1)
+#define NUM_ANIMATIONS 1
+
+typedef unsigned int (animation_t)(unsigned int, LedDisk*);
+
+struct LedDisk {
+  static animation_t * animations[NUM_ANIMATIONS];
+  unsigned int start_idx;
+  unsigned int anim_idx, anim_frame;
+
+  void draw(int idx, uint32_t color) {
+    led_strip.setPixelColor(idx + start_idx, color);
+  }
+
+  void set_animation(unsigned int idx) {
+    anim_idx = idx;
+    anim_frame = 0;
+  }
+
+  void update() {
+    if (anim_idx < NUM_ANIMATIONS) {
+      unsigned int next = animations[anim_idx](anim_frame, this);
+      anim_frame += 1;
+      if (next != anim_idx) {
+        set_animation(next);
+      }
+    }
+  }
+};
+animation_t * LedDisk::animations[NUM_ANIMATIONS];
+
+
+unsigned int spin_anim(unsigned int frame, LedDisk *disk) {
+  int pos = frame % OUTER_DISK_LEN;
+  for (int i=0; i<TOTAL_DISK_LEN; i++) {
+    disk->draw(
+      i, 
+      i == pos ? led_strip.Color(0xff, 0, 0) : led_strip.Color(0,0,0)
+    );
+  }
+  return 0;
+}
+
+
+// --===== timing intervals =====--
+
+typedef struct {
+  unsigned long timestamp = 0;
+  unsigned long delta = 0;
+} interval_t;
+
+
+int interval_ready(interval_t *interval);
+int interval_ready(interval_t *interval) {
+  if (millis() > interval->timestamp) {
+    interval->timestamp = millis() + interval->delta;
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+
+// --===== serial output data dump functions =====--
+
+void dump_button(int pin) {
+  Serial.print(digitalRead(pin) ? 0 : 1023);
+  Serial.print(" ");
+}
+
+void dump_analog(int pin) {
+  Serial.print(analogRead(pin));
+  Serial.print(" ");
+}
+
+void dump_joysticks() {
+  dump_analog(JOY1_X);
+  dump_analog(JOY1_Y);
+  dump_analog(JOY2_Y);
+}
+
+void dump_buttons() {
+  dump_button(UP_BTN);
+  dump_button(DOWN_BTN);
+  dump_button(TARGET1);
+  dump_button(TARGET2);
+  dump_button(TARGET3);
+}
+
+
+
+// --===== globals =====--
+LedDisk disk1, disk2, disk3;
+interval_t ui_update, led_update;
+// led_strip should be here too but is referenced in specific functions
+
+
+
+// --===== setup & loop =====--
 
 void setup() {
-	pinMode(B_UP, INPUT_PULLUP);
-	pinMode(B_DOWN, INPUT_PULLUP);
+  // configure all button pins
+	pinMode(UP_BTN, INPUT_PULLUP);
+	pinMode(DOWN_BTN, INPUT_PULLUP);
+	pinMode(TARGET1, INPUT_PULLUP);
+	pinMode(TARGET2, INPUT_PULLUP);
+	pinMode(TARGET3, INPUT_PULLUP);
+
+	// configure LED strip
+	led_strip.begin();
+	led_strip.show();
+	led_strip.setBrightness(LED_BRIGHTNESS);
+
+	// configure disks
+  LedDisk::animations[0] = spin_anim;
+	disk1.start_idx = 0 * TOTAL_DISK_LEN;
+	disk1.set_animation(0);
+	disk2.start_idx = 1 * TOTAL_DISK_LEN;
+	disk2.set_animation(0);
+	disk3.start_idx = 2 * TOTAL_DISK_LEN;
+	disk3.set_animation(0);
+
+  // enable serial comms
 	Serial.begin(115200);
+
+  // configure update intervals
+	ui_update.delta = 100; 
+	led_update.delta = 10;
 }
 
 
 void loop() {
-	Serial.print(analogRead(J1_X)); Serial.print(" ");
-	Serial.print(analogRead(J1_Y)); Serial.print(" ");
-	Serial.print(analogRead(J2_Y)); Serial.print(" ");
-	Serial.print(digitalRead(B_UP)   ? 0 : 1023); Serial.print(" ");
-	Serial.print(digitalRead(B_DOWN) ? 0 : 1023); Serial.print(" ");
-	Serial.println();
-	delay(100);
+  if (interval_ready(&ui_update)) {
+    dump_joysticks();
+    dump_buttons();
+	  Serial.println();
+	}
+
+	if (interval_ready(&led_update)) {
+  	disk1.update();
+  	disk2.update();
+  	disk3.update();
+  	led_strip.show();
+  }
 }
---
+++ robot-arm-controller/js_firmware/pins.h
@@ -0,0 +1,21 @@
+#ifndef PINS_H
+#define PINS_H
+
+#define BRIGHTSIGN_B0 10
+#define BRIGHTSIGN_B1 9
+#define BRIGHTSIGN_B2 8
+
+#define LED_PIN 6
+
+#define TARGET1 7
+#define TARGET2 5
+#define TARGET3 4
+
+#define UP_BTN 3
+#define DOWN_BTN 2
+
+#define JOY1_X A1
+#define JOY1_Y A0
+#define JOY2_Y A2
+
+#endif
